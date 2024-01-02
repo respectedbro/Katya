ff990ab68daf86b3ea7fcc5f26293b47a8d1897a
NikDm <137294693+respectedbro@users.noreply.github.com>
2024-01-02 15:54:33 +0300

    Add files via upload

--- Kat/script.js
+++ Kat/script.js
@@ -11,88 +11,66 @@ $('.header__inner').toggleClass('header__inner--open')
 
 
 
-$( ".img-wrapper" ).hover(
-  function() {
-    $(this).find(".img-overlay").animate({opacity: 1}, 600);
-  }, function() {
-    $(this).find(".img-overlay").animate({opacity: 0}, 600);
-  }
-);
 
-// Lightbox
-var $overlay = $('<div id="overlay"></div>');
-var $image = $("<img>");
-var $prevButton = $('<div id="prevButton"><i class="fa fa-chevron-left"></i></div>');
-var $nextButton = $('<div id="nextButton"><i class="fa fa-chevron-right"></i></div>');
-var $exitButton = $('<div id="exitButton"><i class="fa fa-times"></i></div>');
 
-// Add overlay
-$overlay.append($image).prepend($prevButton).append($nextButton).append($exitButton);
-$("#gallery").append($overlay);
 
-// Hide overlay on default
-$overlay.hide();
 
-// When an image is clicked
-$(".img-overlay").click(function(event) {
-  // Prevents default behavior
-  event.preventDefault();
-  // Adds href attribute to variable
-  var imageLocation = $(this).prev().attr("href");
-  // Add the image src to $image
-  $image.attr("src", imageLocation);
-  // Fade in the overlay
-  $overlay.fadeIn("slow");
-});
+const galleryItem = document.getElementsByClassName("gallery-item");
+const lightBoxContainer = document.createElement("div");
+const lightBoxContent = document.createElement("div");
+const lightBoxImg = document.createElement("img");
+const lightBoxPrev = document.createElement("div");
+const lightBoxNext = document.createElement("div");
 
-// When the overlay is clicked
-$overlay.click(function() {
-  // Fade out the overlay
-  $(this).fadeOut("slow");
-});
+lightBoxContainer.classList.add("lightbox");
+lightBoxContent.classList.add("lightbox-content");
+lightBoxPrev.classList.add("fa", "fa-angle-left", "lightbox-prev");
+lightBoxNext.classList.add("fa", "fa-angle-right", "lightbox-next");
 
-// When next button is clicked
-$nextButton.click(function(event) {
-  // Hide the current image
-  $("#overlay img").hide();
-  // Overlay image location
-  var $currentImgSrc = $("#overlay img").attr("src");
-  // Image with matching location of the overlay image
-  var $currentImg = $('#image-gallery img[src="' + $currentImgSrc + '"]');
-  // Finds the next image
-  var $nextImg = $($currentImg.closest(".image").next().find("img"));
-  // All of the images in the gallery
-  var $images = $("#image-gallery img");
-  // If there is a next image
-  if ($nextImg.length > 0) { 
-    // Fade in the next image
-    $("#overlay img").attr("src", $nextImg.attr("src")).fadeIn(800);
-  } else {
-    // Otherwise fade in the first image
-    $("#overlay img").attr("src", $($images[0]).attr("src")).fadeIn(800);
-  }
-  // Prevents overlay from being hidden
-  event.stopPropagation();
-});
+lightBoxContainer.appendChild(lightBoxContent);
+lightBoxContent.appendChild(lightBoxImg);
+lightBoxContent.appendChild(lightBoxPrev);
+lightBoxContent.appendChild(lightBoxNext);
 
-// When previous button is clicked
-$prevButton.click(function(event) {
-  // Hide the current image
-  $("#overlay img").hide();
-  // Overlay image location
-  var $currentImgSrc = $("#overlay img").attr("src");
-  // Image with matching location of the overlay image
-  var $currentImg = $('#image-gallery img[src="' + $currentImgSrc + '"]');
-  // Finds the next image
-  var $nextImg = $($currentImg.closest(".image").prev().find("img"));
-  // Fade in the next image
-  $("#overlay img").attr("src", $nextImg.attr("src")).fadeIn(800);
-  // Prevents overlay from being hidden
-  event.stopPropagation();
-});
+document.body.appendChild(lightBoxContainer);
 
-// When the exit button is clicked
-$exitButton.click(function() {
-  // Fade out the overlay
-  $("#overlay").fadeOut("slow");
-});
+let index = 1;
+
+function showLightBox(n) {
+    if (n > galleryItem.length) {
+        index = 1;
+    } else if (n < 1) {
+        index = galleryItem.length;
+    }
+    let imageLocation = galleryItem[index-1].children[0].getAttribute("src");
+    lightBoxImg.setAttribute("src", imageLocation);
+}
+
+function currentImage() {
+    lightBoxContainer.style.display = "block";
+
+    let imageIndex = parseInt(this.getAttribute("data-index"));
+    showLightBox(index = imageIndex);
+}
+for (let i = 0; i < galleryItem.length; i++) {
+    galleryItem[i].addEventListener("click", currentImage);
+}
+
+function slideImage(n) {
+    showLightBox(index += n);
+}
+function prevImage() {
+    slideImage(-1);
+}
+function nextImage() {
+    slideImage(1);
+}
+lightBoxPrev.addEventListener("click", prevImage);
+lightBoxNext.addEventListener("click", nextImage);
+
+function closeLightBox() {
+    if (this === event.target) {
+        lightBoxContainer.style.display = "none";
+    }
+}
+lightBoxContainer.addEventListener("click", closeLightBox);
